bec27d0b1bd32f216f76c394477ab55cc0ed025f
Michael Rosario <mjfrosario@gmail.com>
2019-05-29 14:45:58 -0400

    convert activity to homework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import options from "./options.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    options
+  }
+
+  handleCard = id => {
+    const options = this.state.options.sort(() => Math.random() - 0.5);
+    this.setState({ options });
+  }
+
+  render(){
+
+    const { options } = this.state;
+
+    const cardOptions = options.map(option => (
+      <Card {...option} clickCard={this.handleCard} key={option.id} />
+    ));
+
+    return (
+      <Wrapper>
+        <h1 className="title">Clicky Game</h1>
+        {cardOptions}
+
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Card/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./style.css";
+
+function Card(props) {
+
+  const {
+    image,
+    clickCard
+  } = props;
+
+  var sectionStyle = {
+    backgroundPosition: "center",
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    backgroundImage: `url(${image})`
+  };
+  
+
+  return (
+    <div className="card" onClick={() => clickCard(props.id)} style={ sectionStyle }>
+    </div>
+  );
+}
+
+export default Card;
