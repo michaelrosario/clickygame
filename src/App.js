47fdd5683fed5c9fc03146cd096cf19846c94dfc
Michael Rosario <mjfrosario@gmail.com>
2019-05-30 10:38:36 -0400

    add additional comments

--- src/App.js
+++ src/App.js
@@ -15,6 +15,16 @@ class App extends React.Component {
 
   state = { ...INITIAL_STATE };
 
+  
+
+  shuffleCards = () => {
+    return this.state.options.sort(() => Math.random() - 0.5);
+  }
+
+  /*
+    onClick event that will check whether the card
+    was clicked already
+  */
   handleCard = id => {
   
     let {
@@ -24,35 +34,36 @@ class App extends React.Component {
       guessed
     } = this.state;
 
+    let runStateUpdate = true; // run setState or not
+
     // shuffle order
-    options = options.sort(() => Math.random() - 0.5);
-    let runStateUpdate = true;
+    options = this.shuffleCards();
 
     for (var i in options) {
       
       // find clicked item in the array
       if (options[i].id === id) {
         
-        // check whether it's already selected
+        // if it's already selected
         if(options[i].selected){
           
-          score = 0;
-          guessed = false;
-          this.resetGame();
-          runStateUpdate = false;
-          break; //stop the loop
+          score = 0;              // reset score
+          guessed = false;        // initiate shake animations
+          this.resetGame();       // reset all selected cards
+          runStateUpdate = false; // don't update the state here since reset will set
+          break;                  // stop the loop
         
         // update score and select
         } else {
         
-          score++; // score up
+          score++;                // score up
 
-          // check high score
-          if(score > highScore){
-            highScore = score;
+          
+          if(score > highScore){  // check high score
+            highScore = score;    // update high score
           }
 
-          guessed = true;
+          guessed = true;         
           options[i].selected = true;
 
         }
